3cdc540b8fc4fe0a3ea1664b10648dbb0aab4bd6
In Tak Hwang <sp301415@gmail.com>
2019-08-21 14:28:13 +0900

    Fixed logic

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
 from src import postprocessing
 from gensim.models import Word2Vec
+from sklearn.manifold import TSNE
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
-print(postprocessing.tokenize(2015, 1))
+
+def draw_plot_tsne(model):
+    mpl.rcParams['axes.unicode_minus'] = False
+    mpl.rc('font', family='AppleGothic')
+    vocab = list(model.wv.vocab)
+    X = model[vocab]
+    print(len(X))
+
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=1000)
+    X_tsne = tsne.fit_transform(X)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot(1, 1, 1)
+    df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
+    ax.scatter(df['x'], df['y'])
+
+    for word, pos in df.iterrows():
+        ax.annotate(word, pos)
+    plt.show()
+
+
+result = []
+'''
+for m in range(1, 13):
+    postprocessing.html_2_txt(2014, m)
+    result.extend(postprocessing.tokenize(2014, m))
+'''
+
+# for m in range(1, 2):
+# postprocessing.html_2_txt(2014, m)
+result.extend(postprocessing.tokenize(2014, 1))
+
+if os.path.exists(f"2019.model"):
+    model = Word2Vec.load("2019.model")
+else:
+    model = Word2Vec(result, size=300, min_count=100, workers=4, sg=1)
+    model.save("2019.model")
+
+draw_plot_tsne(model)
--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,27 +1,32 @@
 from bs4 import BeautifulSoup as bs
-from konlpy.tag import Kkma
+from konlpy.tag import Okt
 import glob
 import os
+import re
+
+remove = ["서울신문", "페이스북", "글로벌세상", "동영상뉴스", "전체보기", "포토뉴스", "아시아투데이", "기자", "한국일보",
+          "무단 전재 및 재배포 금지", "서울신문사 무단 전재 및 재배포 금지"]
 
 
 def html_2_txt(year, month):
     if month < 10:
         month = "0" + str(month)
+
     if not os.path.exists(f"./newspaper/txt/newspaper{year}/{year}.{month}"):
         os.makedirs(f"./newspaper/txt/newspaper{year}/{year}.{month}")
 
-    for file in glob.glob(f"./newspaper/newspaper{year}/{year}.{month}/*.html"):
-        with open(file, encoding="euc-kr") as f:
+    for file in os.listdir(f"./newspaper/newspaper{year}/{year}.{month}/"):
+        with open(f"./newspaper/newspaper{year}/{year}.{month}/" + file, encoding="euc-kr") as f:
             soup = bs(f, 'html.parser')
             article = soup.find("p").get_text()
-            if len(article) == 0:
-                break
-            file = file.rstrip(".html")
-            article_to_txt = f"./newspaper/txt/newspaper" + file.lstrip("./newspaper/") + ".txt"
-            for k, s in enumerate(article):
-                if "ⓒ" == s:
-                    article = article[:k]
-                    break
+
+            for r in remove:
+                article.replace(r, "")
+
+            article = re.sub('[^ \nㄱ-ㅣ가-힣]+', "", article)
+
+            article_to_txt = f"./newspaper/txt/newspaper{year}/{year}.{month}/" + file.rstrip(".html") + ".txt"
+
             txt = open(article_to_txt, "w")
             txt.write(article)
             txt.close()
@@ -30,10 +35,19 @@ def html_2_txt(year, month):
 def tokenize(year, month):
     if month < 10:
         month = "0" + str(month)
-    kkma = Kkma()
+    okt = Okt()
     result = []
+    i = 0
     for file in glob.glob(f"./newspaper/txt/newspaper{year}/{year}.{month}/*.txt"):
+        print(i)
+        print(file)
+        i += 1
         f = open(file, "r")
-        result.extend(kkma.nouns(f.readline()))
+        token = okt.pos(f.readline(), stem=True, norm=True)
+        temp = []
+        for word in token:
+            if (word[1] == "Noun") and (len(word[0]) > 1):
+                temp.append(word[0])
+        result.append(temp)
         f.close()
     return result
